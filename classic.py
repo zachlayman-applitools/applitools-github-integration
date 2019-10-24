774544389e905f3998462c35bb67650a2546b879
Zach Layman <zachlayman@zachs-mbp.lan>
2019-10-24 11:32:57 -0500

    uncomment

--- classic.py
+++ classic.py
@@ -21,7 +21,7 @@ class HelloWorld:
         # Open a Chrome browser.
         username = os.environ["SAUCE_USERNAME"]
         access_key = os.environ["SAUCE_ACCESS_KEY"]
-        # capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
+        capabilities["tunnel-identifier"] = os.environ["TRAVIS_JOB_NUMBER"]
         hub_url = "%s:%s@localhost:4445" % (username, access_key)
         driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
 
